75354e194f24698ec11605a900a0090e7d4a291d
hieudtt <danghieu231299@gmail.com>
2024-06-18 09:22:09 +0700

    oauth2

---
+++ google-oauth2/src/main/java/com/google/oauth2/AuthConfig.java
@@ -0,0 +1,7 @@
+package com.google.oauth2;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AuthConfig {
+}
---
+++ google-oauth2/src/main/java/com/google/oauth2/AuthController.java
@@ -0,0 +1,7 @@
+package com.google.oauth2;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthController {
+}
