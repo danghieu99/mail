9ece319a10dab36ee0d812dd049c5f0908d23121
hieudtt <danghieu231299@gmail.com>
2024-06-20 13:20:32 +0700

    multiple attachments

--- kafka-listener/src/main/java/com/example/kafkalistener/mailsenderclient/MailSenderClient.java
+++ kafka-listener/src/main/java/com/example/kafkalistener/mailsenderclient/MailSenderClient.java
@@ -22,7 +22,7 @@ public class MailSenderClient {
                 .host("host.docker.internal")
                 .scheme("http")
                 .port(8081)
-                .path("/api/mail/sendmailjson")
+                .path("/api/mail/personal/sendmailjson")
                 .build()
                 .toUri();
 
