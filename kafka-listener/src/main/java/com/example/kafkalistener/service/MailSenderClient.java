843fa6b1b5d312401591b966a5ac1ca9874e5e97
danghieu99 <danghieu231299@gmail.com>
2024-06-21 21:48:34 +0700

    Revert "Reapply "Delete kafka-listener directory""
    This reverts commit dabfa4a1edc298d39d4f721cfffef2e4a5b58a99.

---
+++ kafka-listener/src/main/java/com/example/kafkalistener/service/MailSenderClient.java
@@ -0,0 +1,41 @@
+package com.example.kafkalistener.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.*;
+import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+
+@Service
+public class MailSenderClient {
+
+    @Autowired
+    private RestClient restClient;
+
+    public String SendMailData(String jsonMail) {
+
+        URI uri = UriComponentsBuilder.newInstance()
+                .host("host.docker.internal")
+                .scheme("http")
+                .port(8081)
+                .path("/api/mail/sendmailjson")
+                .build()
+                .toUri();
+
+        MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
+        body.add("mail", jsonMail);
+
+        ResponseEntity<String> response = restClient.post()
+                .uri(uri)
+                .accept(MediaType.MULTIPART_FORM_DATA)
+                .body(body)
+                .retrieve()
+                .toEntity(String.class);
+
+        return ("Response Status Code: " + response.getStatusCode() + "Response Body: " + response.getBody());
+    }
+}
