fb554c44e38697080f1c461144240dbabe9937ba
danghieu99 <danghieu231299@gmail.com>
2024-06-23 20:21:59 +0700

    .

---
+++ gmailapi/src/test/java/com/demo/gmailapi/GmailapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.gmailapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmailapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
