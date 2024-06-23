35ed61452fcbc4ffe902b66364c490e75da19df4
danghieu99 <danghieu231299@gmail.com>
2024-06-23 22:14:28 +0700

    .

--- mailsender/src/main/java/com/example/mailsender/config/MailSenderConfig.java
+++ mailsender/src/main/java/com/example/mailsender/config/MailSenderConfig.java
@@ -26,6 +26,7 @@ public class MailSenderConfig {
         props.put("mail.transport.protocol", "smtp");
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.sasl.enable", "true");
         props.put("mail.debug", "true");
         props.put("mail.smtp.auth.mechanisms", "XOAUTH2");
 
