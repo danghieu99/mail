9aafbc9c71069c4f9f44adce6cf07b4876b9de19
hieudtt <danghieu231299@gmail.com>
2024-06-28 16:36:59 +0700

    adding scheduled mail

--- kafkamailsender/src/main/java/com/mail/kafkamailsender/config/KafkaMailSenderConfig.java
+++ kafkamailsender/src/main/java/com/mail/kafkamailsender/config/KafkaMailSenderConfig.java
@@ -4,9 +4,7 @@ import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.RestTemplate;
 
--- mailsender/src/main/java/com/example/mailsender/controller/MailServiceController.java
+++ mailsender/src/main/java/com/example/mailsender/controller/MailServiceController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
 
 @RestController
@@ -16,13 +17,22 @@ public class MailServiceController {
     @Autowired
     private MailService mailService;
 
-    @PostMapping("/sendmail")
-    public String sendMailWithAttachments(@RequestParam("from") String from,
-                                          @RequestParam List<String> to,
-                                          @RequestParam("subject") String subject,
-                                          @RequestParam("body") String body,
-                                          @RequestParam(value = "files", required = false) Collection<MultipartFile> files) {
+    @PostMapping("/sendmailwithfiles")
+    public String sendMailWithAttachmentFiles(@RequestParam("from") String from,
+                                              @RequestParam List<String> to,
+                                              @RequestParam("subject") String subject,
+                                              @RequestParam("body") String body,
+                                              @RequestParam(value = "file", required = false) Collection<MultipartFile> files) {
         return mailService.sendMailWithAttachmentFiles(from, to, subject, body, files);
+    }
+
+    @PostMapping("/sendmail")
+    public String sendMail(@RequestParam("from") String from,
+                           @RequestParam List<String> to,
+                           @RequestParam("subject") String subject,
+                           @RequestParam("body") String body,
+                           @RequestParam(value = "files", required = false) HashMap<String, String> attachments) {
+        return mailService.sendMailWithAttachments(from, to, subject, body, attachments);
     }
 
     @PostMapping("/sendmailjson")
