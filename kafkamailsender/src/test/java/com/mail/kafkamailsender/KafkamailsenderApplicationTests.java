a158b622448c4627d69f360507afe6322f19f61b
danghieu99 <danghieu231299@gmail.com>
2024-06-30 22:09:17 +0700

    .

---
+++ kafkamailsender/src/test/java/com/mail/kafkamailsender/KafkamailsenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mail.kafkamailsender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkamailsenderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
